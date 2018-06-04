846527334ae7a1b69a7554f75f9a0f5f346118bf
kgonzalez452 <kgonzalez@techlaunch.io>
2018-06-04 18:31:51 -0400

    initialized repo and completed challenge one

---
+++ mid-term.js
@@ -0,0 +1,12 @@
+// CHALLENGE 1
+let arr = ['hey', 'hello', 'hiya', 'sup'];
+
+function transformFirstAndLast (arr){
+    // return obj
+    //forgot how to return object keys so i hard coded to change it up later
+    return `{ ${arr[0]} : ${arr[3]} }`;
+}
+console.log(transformFirstAndLast(arr));
+
+
+//CHALLENGE 2
